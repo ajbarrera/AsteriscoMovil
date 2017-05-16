b78694d46e2bb23d9681e6e3ebb1996f496f7d6f
antonio barrera <togno3@hotmail.com>
2017-05-16 16:41:14 -0500

    Lectura

--- AsteriscoMovil/AsteriscoMovil.cpp
+++ AsteriscoMovil/AsteriscoMovil.cpp
@@ -1,6 +1,8 @@
 // AsteriscoMovil.cpp : main project file.
 
 #include "stdafx.h"
+#include "conio.h"
+#include "stdio.h"
 
 using namespace System;
 
@@ -9,6 +11,9 @@ int main(array<System::String ^> ^args)
 	int num1, num2;
 	double num3;
 
+	printf("Ingrese un numero: ");
+	scanf("%d", &num1);
+	printf("El numero ingreado es: %d", num1);
 	getch();
     return 0;
 }
