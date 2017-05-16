e437450bec30a2ccee697130faa3692fab3aa744
antonio barrera benetres <antonio barrera benetres>
2017-05-16 15:34:57 -0500

    Add project files.

---
+++ AsteriscoMovil/AsteriscoMovil.cpp
@@ -0,0 +1,14 @@
+// AsteriscoMovil.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+	int num1, num2;
+	double num3;
+
+	getch();
+    return 0;
+}
